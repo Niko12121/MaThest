68d7feb6aaae373f530fc08ec06ef8d5d33ed6c2
Niko12121 <vdonat@uc.cl>
2021-06-25 02:10:57 -0400

    new system to choose questions

--- src/components/question/Question.js
+++ src/components/question/Question.js
@@ -1,26 +1,25 @@
 import './question.css'
 import questions from './questions_ingame'
-import AnswerOption from '../answers/answer_option'
 
 const Question = (props) => {
-
     /*This part randomize the order of answers*/
-    const order = questions[props.level][props.option][1]
-    order.sort(function() { return Math.random() - 0.5 }) 
+    let render_order = questions[props.level][props.option][1]
+    const correct = render_order[0]
+    render_order = render_order.sort(function() { return Math.random() - 0.5 })
+    let to_render = []
+    render_order.forEach(a => to_render.push({text: a, real: correct===a}))
+    console.log(to_render)
 
     return (
         <div class='question'>
             <div class='real_question'>
                 {questions[props.level][props.option][0]}
             </div>
-            {order.map(a => {
-                return (<AnswerOption text={a}/>)
-            })}
+            {to_render.map((a) => (
+                <div class='option' key={a.id}> {a.text} {a.real.toString()}</div>
+            ))}
         </div>
-    )
-}
-
-
+    )}
 
 
 export default Question
--- src/components/test/Test.js
+++ src/components/test/Test.js
@@ -1,17 +1,45 @@
 import Question from "../question/Question"
 
+/* How many questions per level will the page render */
+const amount_value = 2
+/* How many questions per level are already wrote (max_value >= amount_value) */
+const max_value = 3
+/* How many levels exist */
+const level_value = 2
+
+/* This function return a list with amount elements, whit different randoms whole numbers 
+between 0 and max; used like */
+function pickRandoms(amount, max) {
+    let actual = []
+    for (let i = 0; i < amount; i++) {
+        let randomNumber = Math.floor(Math.random() * max)
+        while (actual.includes(randomNumber)) {
+            randomNumber = Math.floor(Math.random() * max)
+        }
+        actual.push(randomNumber)
+    }
+    return actual
+}
+
 const Test = () => {
     let randoms = []
-
-    /* randoms is a list with whole number between 0 and 2, which represent the question of level i
-    which will appear */
-    for (let i=0; i<2; i++) {
-        randoms.push([i, Math.floor(Math.random() * 3)])
+    /* randoms is like [[0, [2, 7, 4]], [1, [1, 5, 2]]], for the level 0, the questions 2, 7 and 4; 
+    for the level 1, questions 1, 5, 2 */
+    for (let i=0; i<level_value; i++) {
+        randoms.push([i, pickRandoms(amount_value, max_value)])
     }
+    let render = []
+    randoms.forEach(a => {
+        render.push(<div>INICIO DE SECCION</div>)
+        a[1].forEach(b => {
+            render.push(<Question level={a[0]} option={b} />)
+        })
+        render.push(<div>TERMINO DE SECCION</div>)
+    })
     return (
         <div>
-            {randoms.map(a => {
-               return (<Question level={a[0]} option={a[1]} />) 
+            {render.map(a => {
+                return <div key={a.id}>{a}</div>
             })}
         </div>
     )
