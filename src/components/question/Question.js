c9490d12a09c9c62a019f4da66e76b2f825674c3
Niko12121 <vdonat@uc.cl>
2021-06-26 03:23:12 -0400

    Know when an answer is correct! and add a className when it is

--- src/components/question/Question.js
+++ src/components/question/Question.js
@@ -2,22 +2,36 @@ import './question.css'
 import questions from './questions_ingame'
 
 const Question = (props) => {
-    /*This part randomize the order of answers*/
+    /* How many options have the tested for question */
+    var options = 1
+
+    /*This part randomize the order of answers and save the correct answer*/
     let render_order = questions[props.level][props.option][1]
     const correct = render_order[0]
     render_order = render_order.sort(function() { return Math.random() - 0.5 })
-    let to_render = []
-    render_order.forEach(a => to_render.push({text: a, real: correct===a}))
-    console.log(to_render)
+
+
+    let corroborate = (event) => {
+        if (options >= 1) {
+            let real = event.target.innerText == correct
+            real ? event.target.classList.add('correct') : event.target.classList.add('wrong')
+            /*real ? event.target.style.backgroundColor = '#00a939' : event.target.style.backgroundColor = '#de3d3d'
+            */
+            options -= 1
+        }
+    }
 
     return (
-        <div class='question'>
+        <div class="question">
             <div class='real_question'>
                 {questions[props.level][props.option][0]}
             </div>
-            {to_render.map((a) => (
-                <div class='option' key={a.id}> {a.text} {a.real.toString()}</div>
-            ))}
+            {render_order.map((a, index) => {
+                return (
+                    <div className='option' onClick={corroborate}>
+                        {a}
+                    </div>
+                )})}
         </div>
     )}
 
--- src/index.js
+++ src/index.js
@@ -4,11 +4,22 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+
+class TodoApp extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  
+  render() {
+    return (
+      <div>
+        <App />
+      </div>
+    )
+  }
+}
+
+
+ReactDOM.render(<TodoApp />,document.getElementById('root'));
 
 reportWebVitals();
